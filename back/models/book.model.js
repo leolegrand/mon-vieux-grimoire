bf24c5925d3194e80186badd296ee81f20517777
leolegrand <88097360+leolegrand@users.noreply.github.com>
2023-06-04 08:37:47 +0200

    update books controller
    - handle file
    - tweaks and fixes

--- back/models/book.model.js
+++ back/models/book.model.js
@@ -16,10 +16,15 @@ const bookSchema = mongoose.Schema({
    averageRating: { type: Number, default: 0 }
 })
 
+// A chaque fois qu'un nouveau livre est sauvegardé sur MongoDB, sa note moyenne est mise à jour
 bookSchema.pre('save', function (next) {
   const ratings = this.ratings.map((rating) => rating.grade)
-  const sumOfRatings = ratings.reduce((acc, curr) => acc + curr, 0)
-  this.averageRating = Math.ceil(sumOfRatings / ratings.length)
+  if (ratings.length === 0) {
+    this.averageRating = 0
+  } else {
+    const sumOfRatings = ratings.reduce((acc, curr) => acc + curr, 0)
+    this.averageRating = Math.ceil(sumOfRatings / ratings.length)
+  }
   next()
 })
 
--- back/routes/book.routes.js
+++ back/routes/book.routes.js
@@ -11,7 +11,7 @@ router.get('/bestrating', bookCtrl.getTopRatedBooks)
 router.get('/:id',  bookCtrl.getOneBook)
 router.post('/', auth, multer, bookCtrl.createBook)
 router.post('/:id/rating', auth, bookCtrl.addBookRating)
-router.put('/:id',  bookCtrl.modifyBook)
-router.delete('/:id',  bookCtrl.deleteBook)
+router.put('/:id', auth, multer, bookCtrl.modifyBook)
+router.delete('/:id', auth, bookCtrl.deleteBook)
 
 module.exports = router
